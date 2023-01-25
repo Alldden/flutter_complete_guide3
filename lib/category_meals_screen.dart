3d26f2f1695fea2fff98b9657dfcb0f36793b834
Alden <aldenhasanovic@gmail.com>
2023-01-25 21:37:40 +0100

    Navigating to a New Page

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('The Recpes'),
+        ),
+        body: Center(
+          child: Text('The Recipes For The Category'),
+        ));
+  }
+}
