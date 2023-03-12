1b203b82770199efc055109b485b663e62cd0a68
Alden <aldenhasanovic@gmail.com>
2023-03-12 22:08:44 +0100

    Selecting Meals for a Chosen Category

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -17,20 +17,21 @@ class Meal {
   final bool isGlutenFree;
   final bool isLactoseFree;
   final bool isVegan;
-  final bool isVegeterian;
+  final bool isVegetarian;
 
-  Meal(
-      {@required this.id,
-      @required this.categories,
-      @required this.title,
-      @required this.imageUrl,
-      @required this.ingredients,
-      @required this.steps,
-      @required this.duration,
-      @required this.complexity,
-      @required this.affordability,
-      @required this.isGlutenFree,
-      @required this.isLactoseFree,
-      @required this.isVegan,
-      @required this.isVegeterian});
+  const Meal({
+    @required this.id,
+    @required this.categories,
+    @required this.title,
+    @required this.imageUrl,
+    @required this.ingredients,
+    @required this.steps,
+    @required this.duration,
+    @required this.complexity,
+    @required this.affordability,
+    @required this.isGlutenFree,
+    @required this.isLactoseFree,
+    @required this.isVegan,
+    @required this.isVegetarian,
+  });
 }
